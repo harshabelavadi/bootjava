05a926f30efd3d090ebcb3acb9a41a87f7128e21
Harsha Belavadi <harsha.ba@philips.com>
2020-08-14 10:44:55 +0530

    No Oggy

--- remover/Main.java
+++ remover/Main.java
@@ -13,7 +13,39 @@ public class Main {
         names.removeAll(Arrays.asList("", null));
         return;
     }
+    public static boolean oggyIsRemoved(Arrimport java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void removeOggy(ArrayList<String> names) {
+        
+        // for (int index = 0; index < names.size(); index++) { 
+        //     // accessing each element of array 
+        //     String name = names.get(index);
+        //     if(name.contains("oggy"))
+        //         names.set(index, "");
+        // }
+        
+        names.removeIf(it -> it.contains("oggy"));
+        // names.forEach(System.out::println);
+        return;
+    }
     public static boolean oggyIsRemoved(ArrayList<String> names) {
+        List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
+        return oggys.size() == 0;
+    }
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
+        removeOggy(names);
+        if(oggyIsRemoved(names)) {
+            System.out.println("Passed!");
+            System.exit(0);
+        } else {
+            System.out.println("Failed!!");
+            System.exit(1);
+        }
+    }
+}ayList<String> names) {
         List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
         return oggys.size() == 0;
     }
